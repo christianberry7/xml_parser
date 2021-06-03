7f65913cb662f4f6ebc6eb2d33dd8ebcec1ec307
abmota23 <77942677+abmota23@users.noreply.github.com>
2021-06-03 12:23:01 -0700

    Add files via upload
    log parser

---
+++ log_processing.java
@@ -0,0 +1,38 @@
+import org.codehaus.plexus.util.StringUtils;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class log_processing {
+
+    public static String parseLogFile(String filename) {
+        String averageTS = "";
+        String averageTJ = "";
+        long TS = 0;
+        long TJ = 0;
+        double count = 0;
+        try {
+            File myObj = new File(filename);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String split[]= StringUtils.split(myReader.nextLine());
+                TS += Long.parseLong(split[0]);
+                TJ += Long.parseLong(split[1]);
+                count++;
+            }
+            averageTS = String.valueOf(TS/count);
+            averageTJ = String.valueOf(TJ/count);
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return "average TS: " + averageTS + " average TJ: " + averageTJ;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(parseLogFile("logs.txt"));
+    }
+
+}
